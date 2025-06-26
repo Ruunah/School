121643d6640b3a3c6b5b7d8306cf3e07fc7583fa
Luna <lunarpb09@gmail.com>
2025-06-26 09:14:39 -0300

    modified:   2025/Jogos/1-pong/.godot/editor/editor_layout.cfg 	modified:   2025/Jogos/1-pong/.godot/editor/filesystem_cache10 	modified:   2025/Jogos/3-space_invaders/.godot/editor/create_recent.Node 	modified:   2025/Jogos/3-space_invaders/.godot/editor/e_bullet.tscn-folding-28ec79aa0ffe0cb483c8e7eaae06d4f7.cfg 	modified:   2025/Jogos/3-space_invaders/.godot/editor/editor_layout.cfg 	modified:   2025/Jogos/3-space_invaders/.godot/editor/enemy.tscn-editstate-75513127553bd195bc133709f1d62ecd.cfg 	modified:   2025/Jogos/3-space_invaders/.godot/editor/enemy.tscn-folding-75513127553bd195bc133709f1d62ecd.cfg 	modified:   2025/Jogos/3-space_invaders/.godot/editor/filesystem_cache10 	modified:   2025/Jogos/3-space_invaders/.godot/editor/filesystem_update4 	modified:   2025/Jogos/3-space_invaders/.godot/editor/main.tscn-editstate-2ddd04d543d5858e7f20c9299a8b5096.cfg 	modified:   2025/Jogos/3-space_invaders/.godot/editor/main.tscn-folding-2ddd04d543d5858e7f20c9299a8b5096.cfg 	modified:   2025/Jogos/3-space_invaders/.godot/editor/p_bullet.tscn-editstate-e9c7fc49d5fa671e99920cf6c6a9e28b.cfg 	modified:   2025/Jogos/3-space_invaders/.godot/editor/p_bullet.tscn-folding-e9c7fc49d5fa671e99920cf6c6a9e28b.cfg 	modified:   2025/Jogos/3-space_invaders/.godot/editor/player.tscn-editstate-10d6ba27c95124e9e2f66cb8ebb57635.cfg 	modified:   2025/Jogos/3-space_invaders/.godot/editor/project_metadata.cfg 	modified:   2025/Jogos/3-space_invaders/.godot/editor/script_editor_cache.cfg 	new file:   2025/Jogos/3-space_invaders/.godot/imported/Animation 1(1).png-567302aa2219517f6c3518edde806e89.ctex 	new file:   2025/Jogos/3-space_invaders/.godot/imported/Animation 1(1).png-567302aa2219517f6c3518edde806e89.md5 	new file:   2025/Jogos/3-space_invaders/.godot/imported/Animation 1.png-2c4d24b18dee396442272201061b0f98.ctex 	new file:   2025/Jogos/3-space_invaders/.godot/imported/Animation 1.png-2c4d24b18dee396442272201061b0f98.md5 	new file:   2025/Jogos/3-space_invaders/.godot/imported/Animation 1.png-7fefa60ddad8a6371ac597ae95c75ad1.ctex 	new file:   2025/Jogos/3-space_invaders/.godot/imported/Animation 1.png-7fefa60ddad8a6371ac597ae95c75ad1.md5 	new file:   2025/Jogos/3-space_invaders/.godot/imported/Explosion.png-4cec69c6d6c03527ba22cbc120bf0a6e.md5 	new file:   2025/Jogos/3-space_invaders/.godot/imported/explosion.png-03dbcc1860a0b88a11042db44c8cc013.ctex 	new file:   2025/Jogos/3-space_invaders/.godot/imported/explosion.png-03dbcc1860a0b88a11042db44c8cc013.md5 	modified:   2025/Jogos/3-space_invaders/.godot/scene_groups_cache.cfg 	modified:   2025/Jogos/3-space_invaders/.godot/uid_cache.bin 	modified:   2025/Jogos/3-space_invaders/Prefabs/enemy.tscn 	modified:   2025/Jogos/3-space_invaders/Prefabs/p_bullet.tscn 	modified:   2025/Jogos/3-space_invaders/Prefabs/player.tscn 	modified:   2025/Jogos/3-space_invaders/Scenes/main.tscn 	new file:   2025/Jogos/3-space_invaders/Scenes/main.tscn1445151400.tmp 	new file:   2025/Jogos/3-space_invaders/Scenes/main.tscn1475955915.tmp 	new file:   2025/Jogos/3-space_invaders/Scenes/main.tscn1490181883.tmp 	modified:   2025/Jogos/3-space_invaders/Scripts/enemy.gd 	modified:   2025/Jogos/3-space_invaders/Scripts/p_bullet.gd 	modified:   2025/Jogos/3-space_invaders/Scripts/player.gd 	new file:   2025/Jogos/3-space_invaders/Sprites/explosion.png 	new file:   2025/Jogos/3-space_invaders/Sprites/explosion.png.import

--- 2025/Jogos/3-space_invaders/Scripts/enemy.gd
+++ 2025/Jogos/3-space_invaders/Scripts/enemy.gd
@@ -1,25 +1,17 @@
 extends CharacterBody2D
 
+const SPEED = 200
+var direction
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+func _ready():
+	direction = Vector2(1,0)
+	$AnimationPlayer.play("Idle")
 
-
-func _physics_process(delta: float) -> void:
-	# Add the gravity.
-	if not is_on_floor():
-		velocity += get_gravity() * delta
-
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+func _physics_process(_delta):
+	if get_slide_collision_count()>0:
+		var colision = get_slide_collision(0)
+		direction = direction.bounce(colision.get_normal())
+		
+	velocity = SPEED*direction
 
 	move_and_slide()
--- 2025/Jogos/3-space_invaders/Scripts/p_bullet.gd
+++ 2025/Jogos/3-space_invaders/Scripts/p_bullet.gd
@@ -1,25 +1,12 @@
 extends CharacterBody2D
 
-
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
-
-
-func _physics_process(delta: float) -> void:
-	# Add the gravity.
-	if not is_on_floor():
-		velocity += get_gravity() * delta
-
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-
+func _physics_process(_delta):
+	if get_slide_collision_count()>0:
+		var colision=get_slide_collision(0)
+		if colision.get_collider().name == "Walls":
+			queue_free()
+		elif colision.get_collider().name == "Enemy":
+			colision.$AnimationPlayer.play(Explosion)
+			queue_free()
+	velocity.y = -300
 	move_and_slide()
