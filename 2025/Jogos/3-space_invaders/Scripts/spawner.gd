f7b21e7f0b34f4a0f2e246e54fd093833542a65f
Luna <lunarpb09@gmail.com>
2025-07-16 08:07:17 -0300

    modified:   2025/Jogos/1-pong/.godot/editor/project_metadata.cfg 	modified:   2025/Jogos/3-space_invaders/.godot/editor/editor_layout.cfg 	modified:   2025/Jogos/3-space_invaders/.godot/editor/enemy.tscn-editstate-75513127553bd195bc133709f1d62ecd.cfg 	modified:   2025/Jogos/3-space_invaders/.godot/editor/filesystem_cache10 	modified:   2025/Jogos/3-space_invaders/.godot/editor/filesystem_update4 	modified:   2025/Jogos/3-space_invaders/.godot/editor/main.tscn-editstate-2ddd04d543d5858e7f20c9299a8b5096.cfg 	modified:   2025/Jogos/3-space_invaders/.godot/editor/player.tscn-editstate-10d6ba27c95124e9e2f66cb8ebb57635.cfg 	modified:   2025/Jogos/3-space_invaders/.godot/editor/player.tscn-folding-10d6ba27c95124e9e2f66cb8ebb57635.cfg 	modified:   2025/Jogos/3-space_invaders/.godot/editor/project_metadata.cfg 	modified:   2025/Jogos/3-space_invaders/.godot/editor/script_editor_cache.cfg 	modified:   2025/Jogos/3-space_invaders/.godot/scene_groups_cache.cfg 	modified:   2025/Jogos/3-space_invaders/.godot/uid_cache.bin 	modified:   2025/Jogos/3-space_invaders/Prefabs/enemy.tscn 	modified:   2025/Jogos/3-space_invaders/Prefabs/player.tscn 	modified:   2025/Jogos/3-space_invaders/Scripts/spawner.gd

--- 2025/Jogos/3-space_invaders/Scripts/spawner.gd
+++ 2025/Jogos/3-space_invaders/Scripts/spawner.gd
@@ -1,18 +1,22 @@
 extends Control
 var prefab
-var poshh = 80
-var posv = 144
-var posh = 80
+var anchor_x = 80
+var x = 80
+var y = 144
 func _ready():
 	prefab = preload("res://Prefabs/enemy.tscn")
-	for i in range(2):
-		for v in range(2):
-			for h in range(8):
-				var enemy = prefab.instantiate()
-				get_tree().get_root().add_child.call_deferred(enemy)
-				enemy.position = Vector2(posh, posv)
-				posh+=64
-			posv+=32
-			poshh+=32
-			posh=poshh
-		posv+=32
+	for v in range(4):
+		for h in range(8):
+			var enemy = prefab.instantiate()
+			get_tree().get_root().add_child.call_deferred(enemy)
+			enemy.position = Vector2(x, y)
+			x+=64
+		match v%2:
+			0:
+				anchor_x+=32
+				x=anchor_x
+				y+=64
+			1:
+				anchor_x-=32
+				x=anchor_x
+				y+=64
