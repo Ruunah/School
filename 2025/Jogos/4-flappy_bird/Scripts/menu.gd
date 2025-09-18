f2acd0c6c9117745efad4dd00e1eda82a6fd67b0
Luna <lunarpb09@gmail.com>
2025-09-18 09:08:01 -0300

    Remove 5

--- 2025/Jogos/4-flappy_bird/Scripts/menu.gd
+++ 2025/Jogos/4-flappy_bird/Scripts/menu.gd
@@ -16,18 +16,19 @@ func update():
 		text[2] = "Play Again"
 	if Last_Score >= High_Score:
 		High_Score = Last_Score
+	text = ["Flappy Bird", (("High_Score:"+(str(High_Score) if High_Score else "0")+"\nLast_Score:"+(str(Last_Score) if Last_Score else "0"))), "Play", "Exit"]
 	$menu/Main_Text.text = text[0]
 	$menu/Secondary_Text.text = text[1]
 	$menu/Button1.text = text[2]
 	$menu/Button2.text = text[3]
 
 func _ready():
-	text = ["Flappy Bird", (("High_Score:"+(str(High_Score) if High_Score else "0")+"\nLast_Score:"+(str(Last_Score) if Last_Score else "0"))), "Play", "Exit"]
 	PlayedNow = 0
 	var result = saveIO.load("res://save.json")
 	Played = result[0]
 	High_Score = result[1]
 	Last_Score = result[2]
+	text = ["Flappy Bird", (("High_Score:"+(str(High_Score) if High_Score else "0")+"\nLast_Score:"+(str(Last_Score) if Last_Score else "0"))), "Play", "Exit"]
 
 func play():
 	if not has_node("main"):
@@ -40,7 +41,6 @@ func play():
 				child.hide()
 
 func quit():
-	if not has_node("main"):
-		var save_list = [Played, High_Score, Last_Score]
-		saveIO.save(save_list, "res://save.json")
-		get_tree().quit()
+	var save_list = [Played, High_Score, Last_Score]
+	saveIO.save(save_list, "res://save.json")
+	get_tree().quit()
